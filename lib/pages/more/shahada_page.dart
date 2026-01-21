751f7599d107e7ac6b0361d36397ede09a2db037
Ergashev Nurmukhammad <ergashev.nurik01@mail.ru>
2026-01-21 12:34:13 +0200

    Shahada image added

--- lib/pages/more/shahada_page.dart
+++ lib/pages/more/shahada_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../../l10n/app_localizations.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ShahadaPage extends StatelessWidget {
   const ShahadaPage({super.key});
@@ -12,43 +14,54 @@ class ShahadaPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(t.shahadaTitle),
       ),
-      body: Padding(
-        padding: const EdgeInsetsDirectional.all(24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              t.shahadaArabic,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Amiri',
-                fontSize: 30,
-                height: 1.8,
-                color: Colors.green[900],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsetsDirectional.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+              SvgPicture.asset(
+                'assets/images/shahada.svg',
+                width: 250,
               ),
-            ),
 
-            const SizedBox(height: 32),
+              // Text(
+              //   t.shahadaArabic,
+              //   textAlign: TextAlign.center,
+              //   style: TextStyle(
+              //     fontFamily: 'Amiri',
+              //     fontSize: 30,
+              //     height: 1.8,
+              //     color: Colors.green[900],
+              //   ),
+              // ),
 
-            Text(
-                "Ashhadu an lā ilāha illā-llāh, wa ashhadu anna Muḥammadan rasūlu-llāh",
-                style: TextStyle(
-                  color: Colors.grey
-                ),
+              const SizedBox(height: 32),
+
+              Text(
+                  "Ashhadu an lā ilāha illā-llāh, wa ashhadu anna Muḥammadan rasūlu-llāh",
+                  style: TextStyle(
+                    color: Colors.grey
+                  ),
+                  textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 32),
+
+              Text(
+                t.shahadaTranslation,
                 textAlign: TextAlign.center,
-            ),
-
-            const SizedBox(height: 32),
-
-            Text(
-              t.shahadaTranslation,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 12,
-                height: 1.6,
+                style: TextStyle(
+                  fontSize: 12,
+                  height: 1.6,
+                  color: Colors.green[900]
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
